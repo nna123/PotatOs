806353948e61a2ed9a422cc8cb23bc20a93f9686
ManexXd <74118310+ManexXd@users.noreply.github.com>
2020-11-09 11:21:32 +0100

    Create PotatSystem.h

---
+++ Headers/PotatSystem.h
@@ -0,0 +1,15 @@
+#ifndef POTATOSYSTEM_H_INCLUDED
+#define POTATOSYSTEM_H_INCLUDED
+
+const char* k_n = "PotatOs Kernel";
+const char* k_v = "v0.1.0";
+
+const char* kernel_name() {
+    return k_n;
+}
+
+const char* kernel_version() {
+    return k_v;
+}
+
+#endif // POTATOSYSTEM_H_INCLUDED
